a85c500d9c13fc836a6c6a61e94cfe015dea2dc4
rodrigoh00per <rodrigoh00per1@gmail.com>
2018-11-13 00:25:13 -0600

    Version Completa 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+import { FormsModule } from '@angular/forms';
+import { ComidasComponent } from './components/comidas/comidas.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { RegistrarplatilloComponent } from './components/registrarplatillo/registrarplatillo.component';
+import { DetallecomidaComponent } from './components/detallecomida/detallecomida.component';
+import { ActualizarComponent } from './components/actualizar/actualizar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComidasComponent,
+    InicioComponent,
+    RegistrarplatilloComponent,
+    DetallecomidaComponent,
+    ActualizarComponent
   ],
   imports: [
     BrowserModule
+    , routing,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
